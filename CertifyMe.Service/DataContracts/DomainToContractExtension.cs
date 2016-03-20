2ecf09587d8c71e054a48d742c81f50a8aafdb49
andrii.konovalenko <andreyk6@gmail.com>
2016-03-20 13:46:04 +0200

    Add extension methods for conversion from Domain Model to Data Contract

---
+++ CertifyMe.Service/DataContracts/DomainToContractExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CertifyMe.Service.DataContracts
+{
+    public static class DomainToContractExtension
+    {
+        public static User ToUserContract(this Data.User user)
+        {
+            return new User(user.FirstName, user.LastName, user.Age)
+            {
+                Id = user.Id,
+                CreationTime = user.CreationTime
+            };
+        }
+        public static Certificate ToCertificateContract(this Data.Certificate certificate)
+        {
+            return new Certificate()
+            {
+                Id = certificate.Id,
+                EventId = certificate.Event.Id,
+                OwnerId = certificate.Owner.Id,
+                Title = certificate.Title,
+                Place = certificate.Place,
+                CreationTime = certificate.CreationTime
+            };
+        }
+        public static Company ToCompanyContract(this Data.Company compamy)
+        {
+            return new Company()
+            {
+                Id = compamy.Id,
+                OwnerId = compamy.Owner.Id,
+                CreationTime = compamy.CreationTime,
+                Name = compamy.Name,
+                Description = compamy.Description
+            };
+        }
+        public static Event ToEventContract(this Data.Event @event)
+        {
+            return new Event()
+            {
+                Id = @event.Id,
+                CreationTime = @event.CreationTime,
+                StartDate = @event.StartDate,
+                EndDate = @event.EndDate,
+                Location = @event.Location,
+                Name = @event.Name,
+                Description = @event.Description
+            };
+        }
+        public static EventComment ToEventComment(this Data.EventComment eventComment)
+        {
+            return new EventComment()
+            {
+                Id = eventComment.Id,
+                CreationTime = eventComment.CreationTime,
+                EventId = eventComment.Event.Id,
+                CommentatorId = eventComment.Commentator.Id,
+                Text = eventComment.Text
+            };
+        }
+        public static EventComment ToEventCommentContract(this Data.EventComment comment)
+        {
+            return new EventComment()
+            {
+                Id = comment.Id,
+                CommentatorId = comment.Commentator.Id,
+                EventId = comment.Event.Id,
+                CreationTime = comment.CreationTime,
+                Text = comment.Text
+            };
+        }
+    }
+}
