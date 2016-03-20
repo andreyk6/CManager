035046f34cbb4c9fef6578bc95d55cb02ef71367
andrii.konovalenko <andreyk6@gmail.com>
2016-03-20 08:41:08 +0200

    Add WCF Service reference to Client project

--- CertifyMe.Client/MainWindow.xaml.cs
+++ CertifyMe.Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CertifyMe.Client.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,15 @@ namespace CertifyMe.Client
         public MainWindow()
         {
             InitializeComponent();
+            User user = new User();
+            user.Age = 10;
+            user.FirstName = "Andrii";
+            user.LastName = "Konovalenko";
+            var userService = new UserServiceClient();
+            user.Id = userService.Add(user);
+
+            var userFromServer = userService.GetById(user.Id);
+            //var user = userService; 
         }
     }
 }
