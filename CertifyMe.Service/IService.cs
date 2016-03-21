4c698217b23ecc7a2a5ba7e88c35967be3ed63f2
andrii.konovalenko <andreyk6@gmail.com>
2016-03-21 11:15:58 +0200

    Add base service interface - IService.cs

---
+++ CertifyMe.Service/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace CertifyMe.Service
+{
+
+    [ServiceContract]
+    public interface IService<T> where T: class
+    {
+        [OperationContract]
+        T GetById(Guid id);
+
+        [OperationContract]
+        Guid Add(T item);
+
+        [OperationContract]
+        bool RemoveById(Guid id);
+
+        [OperationContract]
+        bool Update(T item);
+
+        [OperationContract]
+        List<T> GetAll();
+    }
+}
