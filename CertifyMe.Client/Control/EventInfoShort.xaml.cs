29b11df03f390ad73b35d1a637dfb116c7309a46
andrii.konovalenko <andreyk6@gmail.com>
2016-04-10 09:31:44 +0300

    Add EventInfoShort UserControl

---
+++ CertifyMe.Client/Control/EventInfoShort.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CertifyMe.Client.EventServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CertifyMe.Client.Control
+{
+    /// <summary>
+    /// Interaction logic for EventInfoShort.xaml
+    /// </summary>
+    public partial class EventInfoShort : UserControl
+    {
+        public EventInfoShort()
+        {
+            InitializeComponent();
+        }
+
+        private void Details_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((this.DataContext as Event).Name);
+        }
+    }
+}
