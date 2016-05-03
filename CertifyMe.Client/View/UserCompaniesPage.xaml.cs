fa1853648a01560e7414f736369bd4faa10400f8
andrii.konovalenko <andreyk6@gmail.com>
2016-05-03 19:43:33 +0300

    Implement Companies loading at UserCompaniesViewModel

--- CertifyMe.Client/View/UserCompaniesPage.xaml.cs
+++ CertifyMe.Client/View/UserCompaniesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CertifyMe.Client.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace CertifyMe.Client.View
         public UserCompaniesPage()
         {
             InitializeComponent();
+            DataContext = new UserCompaniesViewModel();
         }
     }
 }
--- CertifyMe.Client/ViewModel/UserCompaniesViewModel.cs
+++ CertifyMe.Client/ViewModel/UserCompaniesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CertifyMe.Client.CompanyServiceReference;
+using CertifyMe.Client.EventServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,35 @@ namespace CertifyMe.Client.ViewModel
 {
     public class UserCompaniesViewModel : ViewModelBase
     {
+        protected EventServiceClient _eventClient = new EventServiceClient();
+        CompanyServiceClient _companyClient = new CompanyServiceClient();
 
+        Company[] _companies;
+        public Company[] Companies
+        {
+            get { return _companies; }
+            set
+            {
+                if (_companies == value)
+                    return;
+                _companies = value;
+                OnPropertyChanged("Companies");
+            }
+        }
+
+        public UserCompaniesViewModel()
+        {
+            SystemUser.Instance.PropertyChanged += (o, args) =>
+            {
+                if (args.PropertyName == "Id") LoadCompaniesList();
+            };
+
+            LoadCompaniesList();
+        }
+
+        public virtual void LoadCompaniesList()
+        {
+            Companies = _companyClient.GetUserCompanies(SystemUser.Instance.Id);
+        }
     }
 }
